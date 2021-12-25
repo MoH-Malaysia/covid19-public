e83b38b531c4794a884fa116a5593c23b2b4e756
rogerteoh <rt415@ic.ac.uk>
2021-12-25 23:09:16 +0000

    Added analytics for Malaysia's booster campaign progress

--- analytics/vaccinations.py
+++ analytics/vaccinations.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 from matplotlib.ticker import FuncFormatter
 
 file_path_vaccination = "vaccination/vax_malaysia.csv"
@@ -22,6 +23,13 @@ class Vaccinations:
         df_vaccination["cumul_pfizer_2"] = df_vaccination["pfizer2"].cumsum()
         df_vaccination["cumul_sino_2"] = df_vaccination["sinovac2"].cumsum()
         df_vaccination["cumul_az_2"] = df_vaccination["astra2"].cumsum()
+        df_vaccination["cumul_pfizer_3"] = df_vaccination["pfizer3"].cumsum()
+        df_vaccination["cumul_sino_3"] = df_vaccination["sinovac3"].cumsum()
+        df_vaccination["cumul_az_3"] = df_vaccination["astra3"].cumsum()
+        df_vaccination["cumul_others_3"] = (
+            df_vaccination["cumul_booster"] - df_vaccination["cumul_pfizer_3"] -
+            df_vaccination["cumul_az_3"] - df_vaccination["cumul_sino_3"]
+        )
         return df_vaccination
 
     def plot_daily_vaccines_administered(self, *, percentage: bool = True):
@@ -44,10 +52,12 @@ class Vaccinations:
             df_pct["Pfizer-BioNTech"] = df_vax_weekly["Pfizer-BioNTech"] / df_vax_weekly.sum(axis=1)
             df_pct["Oxford-AstraZeneca"] = df_vax_weekly["Oxford-AstraZeneca"] / df_vax_weekly.sum(axis=1)
             df_pct["Sinovac"] = df_vax_weekly["Sinovac"] / df_vax_weekly.sum(axis=1)
-            df_pct["Others"] = df_vax_weekly["Others"] / df_vax_weekly.sum(axis=1)
+            df_pct["Others/Pending"] = df_vax_weekly["Others"] / df_vax_weekly.sum(axis=1)
             df_pct.index = df_vax_weekly.index.copy()
-            df_pct.iloc[:-1].plot.area(ax=ax_1, y=["Pfizer-BioNTech", "Oxford-AstraZeneca", "Sinovac", "Others"],
-                                       color=custom_colors)
+            df_pct.iloc[:-1].plot.area(
+                ax=ax_1, y=["Pfizer-BioNTech", "Oxford-AstraZeneca", "Sinovac", "Others/Pending"],
+                color=custom_colors
+            )
             ax_1.yaxis.set_major_formatter(FuncFormatter(lambda y, _: '{:.0%}'.format(y)))
             ax_1.set_ylim([0, 1])
 
@@ -56,11 +66,47 @@ class Vaccinations:
             ax_1.axvline(date_booster, color="k", linestyle=":", linewidth=2)
             ax_1.text((date_booster - pd.Timedelta(days=10)), 0.25, 'Boosters offered', rotation=90)
         else:
-            df_vax_weekly.iloc[:-1].plot.area(ax=ax_1, y=["Pfizer-BioNTech", "Oxford-AstraZeneca", "Sinovac", "Others"],
-                                              color=custom_colors)
+            df_vax_weekly.iloc[:-1].plot.area(
+                ax=ax_1, y=["Pfizer-BioNTech", "Oxford-AstraZeneca", "Sinovac", "Others/Pending"],
+                color=custom_colors
+            )
 
         ax_1.set_xlabel(" ", fontsize=1)
         ax_1.set_title("Malaysia: Weekly vaccines administered")
         plt.legend(bbox_to_anchor=(1.01, 0.65))
         plt.tight_layout()
         return
+
+    def plot_booster_campaign_progress(self):
+        # TODO: This will be updated after the new announcement
+        sino_eligible = self.data["cumul_sino_2"].shift(90)
+        pfizer_eligible = self.data["cumul_pfizer_2"].shift(180)
+        az_eligible = self.data["cumul_az_2"].shift(180)
+        tot_eligible = sino_eligible + pfizer_eligible + az_eligible
+
+        df_plot = pd.DataFrame()
+        df_plot["Eligible"] = tot_eligible.values
+        df_plot["Pfizer-BioNTech"] = self.data["cumul_pfizer_3"].values
+        df_plot["Oxford-AZ"] = self.data["cumul_az_3"].values
+        df_plot["Sinovac"] = self.data["cumul_sino_3"].values
+        df_plot["Others"] = self.data["cumul_others_3"].values
+        df_plot.index = self.data["date"]
+
+        start_date = pd.to_datetime("2021-10-25")
+        is_after_date = (self.data["date"] > start_date).values
+        df_plot = df_plot[is_after_date].copy()
+
+        fig = plt.figure(figsize=(5, 4.5), facecolor='w', dpi=125)
+        ax_1 = plt.subplot(1, 1, 1)
+        df_plot.plot(ax=ax_1, y=["Eligible"], linestyle="--", color="k", legend=True)
+        df_plot.plot.area(ax=ax_1, y=["Pfizer-BioNTech", "Sinovac", "Oxford-AZ", "Others"],
+                          color=["dodgerblue", "r", "orange", "lightgrey"], legend=True)
+        plt.ylim([0, 1.25*df_plot["Eligible"].max()])
+        ax_1.yaxis.set_major_formatter(mpl.ticker.StrMethodFormatter('{x:,.0f}'))
+        ax_1.set_xlabel(" ", fontsize=1)
+        ax_1.set_title("Malaysia: Booster doses administered")
+        plt.tight_layout()
+        return
+
+    # TODO: Boosters by age group
+    
